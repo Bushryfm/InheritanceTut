955f666ffb2f384cda59ba79ab0e83de9c8eb1fb
F m Bushry <121561467+Bushryfm@users.noreply.github.com>
2024-08-27 20:36:17 +0530

    Add files via upload

---
+++ Inheritance/Inheritance/College.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class College
+    {
+        string CollegeName = "National college";
+       internal string PrincipalName = "Bushry";
+
+        public void ReplaceMethod() 
+        {
+            Console.WriteLine("The College Name Is " + CollegeName);
+            Console.WriteLine("The Pricipal Name Is " + PrincipalName);
+
+        }
+
+    }
+
+   internal class Department :College
+    {
+       public String DepartmentName = "Science";
+        public String SubDepartmentName = "SpaceScience";
+
+        public void DefaultMethod()
+        {
+            Console.WriteLine("The Department Name is " + DepartmentName);
+            Console.WriteLine("The Sub Department name is " + SubDepartmentName);
+            Console.WriteLine("The Principal name is " + PrincipalName);
+
+        }
+    }
+
+
+
+}
